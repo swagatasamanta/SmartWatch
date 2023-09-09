03ae45a510d9b2fb7620a682073c51609237ba3c
Swagata Samanta <113469289+swagatasamanta@users.noreply.github.com>
2023-09-09 18:02:53 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// const header = document.querySelector("header");
+
+// window.addEventListener("scroll",function(){
+//     header.classList.toggle("sticky", this.window.scroll > 0);
+// })
+
+// let menu = document.querySelector('#menu-icon');
+// let navmenu = document.querySelector('.navmenu');
+
+// menu.onclink = () =>{
+//     menu.classList.toggle('bx-x');
+//     navmenu.classList.toggle('open');
+// }
